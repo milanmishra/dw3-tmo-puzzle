8efef7e7b4f22e8f66881dc05e9bb56007a27c31
Milan <milan.mishra@infosys.com>
2022-08-18 22:17:13 +0530

    [Task-1] - Add code review and fix tests

--- libs/books/data-access/src/lib/+state/books.actions.ts
+++ libs/books/data-access/src/lib/+state/books.actions.ts
@@ -2,18 +2,18 @@ import { createAction, props } from '@ngrx/store';
 import { Book } from '@tmo/shared/models';
 
 export const searchBooks = createAction(
-  '[Books Search Bar] Search',
+  '[Books Search API] Search Books',
   props<{ term: string }>()
 );
 
 export const searchBooksSuccess = createAction(
-  '[Book Search API] Search success',
+  '[Books Search API] Search Books Success',
   props<{ books: Book[] }>()
 );
 
 export const searchBooksFailure = createAction(
-  '[Book Search API] Search failure',
+  '[Books Search API] Search Books Failure',
   props<{ error: any }>()
 );
 
-export const clearSearch = createAction('[Books Search Bar] Clear Search');
+export const clearSearch = createAction('[Books Search API] Clear Search');
